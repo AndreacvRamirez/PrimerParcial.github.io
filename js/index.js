7a220312968972a3e2a4a0222093633a9370dc7b
AndreacvRamirez <80366317+AndreacvRamirez@users.noreply.github.com>
2021-04-15 08:15:50 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
-const Mocknoticias = "http://demo6497253.mockable.io/noticias";
-const noticias = document.querySelector(".noticias");
-const Mockdeportes = "http://demo6497253.mockable.io/categoria/deporte";
-const deporte = document.querySelector(".noticias");
+
+function cargarJsonNoticias() {
+    var url = "http://demo6497253.mockable.io/noticias";
+  
+    fetch(url)
+        .then(response => response.json())
+        .then(data => generarNoticiasInicio(data))
+        .catch(error => {});
+  }
+  
+  function generarNoticiasInicio(json) {
+    const noticias = document.getElementById("noticias");
+  
+    for (let i in json) {
+  
+        noticias.innerHTML += `<div><h1>${json[i].titulo}- ${json[i].categoria} - ${json[i].fecha} </h1>
+                            <h3>${json[i].descripcion}</h3>
+                            <p>${json[i].detalle}</p>
+                            <img src='${json[i].img}'></img></div><hr>`
+    }
+  }
+  cargarJsonNoticias();
 
 
-function getData(url){
-    return fetch(url)
-    .then(response => response.json())
-    .then(json => json)
-}
+
+
